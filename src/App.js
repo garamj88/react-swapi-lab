bcdb8fd324a1e75d3c1fa041883e1e7b4a306fda
Garam Jang <garamj88@gmail.com>
2022-03-17 11:59:10 -0400

    fix fragment

--- src/App.js
+++ src/App.js
@@ -5,12 +5,11 @@ import StartshipListfrom from './pages/StarshipList/StarshipList';
 
 function App() {
   return (
-    <div>
-      <h1>Hi</h1>
+    <>
       <routes>
       <Route path='/startship-list' element={<StartshipList />} />
       </routes>
-    </div>
+    </>
     );
 }
 
